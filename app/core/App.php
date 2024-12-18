3647907473e3af0be5c6cbb8e6eb7d09b55f70bb
Mounir <lm_melzi@esi.dz>
2024-12-18 23:18:19 +0100

    create the router

--- app/core/App.php
+++ app/core/App.php
@@ -1,13 +1,37 @@
 <?php
 
 require_once __DIR__ . "/../config/app.config.php";
+require_once __DIR__ . "/../controllers/HomeController.php";
 
 class App
 {
-    public function __construct() {}
+    private Router $router;
+
+    public function __construct()
+    {
+        $this->router = new Router();
+        $this->setRoutes();
+    }
 
     public function run()
     {
-        echo "App is running";
+        $this->router->dispatch();
+    }
+
+    private function setRoutes()
+    {
+        $this->router->addRoute(
+            'GET',
+            '/',
+            HomeController::class,
+            'index'
+        );
+
+        $this->router->addRoute(
+            'GET',
+            '/home',
+            HomeController::class,
+            'index'
+        );
     }
 }
--- app/core/Router.php
+++ app/core/Router.php
@@ -2,5 +2,50 @@
 
 class Router
 {
-    public function __construct() {}
+    private array $routes = [];
+
+    public function addRoute(
+        string $method,
+        string $path,
+        string $controller,
+        string $action,
+    ) {
+        $this->routes[] = [
+            'method' => strtoupper($method),
+            'path' => trim($path, '/'),
+            'controller' => $controller,
+            'action' => $action,
+        ];
+    }
+
+    public function dispatch()
+    {
+        $url = trim(parse_url($_GET['url'] ?? "/", PHP_URL_PATH), '/');
+
+        foreach ($this->routes as $route) {
+            if ($route['method'] !== $_SERVER['REQUEST_METHOD']) continue;
+
+            $pattern = preg_replace('/\{([a-zA-Z0-9_]+)\}/', '(?P<$1>[^/]+)', $route['path']);
+
+            if (preg_match("#^$pattern$#", $url, $matches)) {
+                $params = array_filter(
+                    $matches,
+                    fn($key) => !is_numeric($key),
+                    ARRAY_FILTER_USE_KEY
+                );
+
+                if (class_exists($route['controller']) && method_exists($route['controller'], $route['action'])) {
+                    call_user_func_array([new $route['controller'], $route['action']], $params);
+                    return;
+                } else {
+                    http_response_code(500);
+                    echo "Controller or action not found.";
+                    return;
+                }
+            }
+        }
+
+        http_response_code(404);
+        echo "Route not found.";
+    }
 }
