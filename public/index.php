e0c52e77e3c95b4be499e7935ad383c41f8d8c20
Mounir <lm_melzi@esi.dz>
2024-12-23 15:20:25 +0100

    set the cache control headers

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 require_once __DIR__ . "/../vendor/autoload.php";
 require_once __DIR__ . "/../app/autoload.php";
 require_once __DIR__ . "/../app/config/router.config.php";
