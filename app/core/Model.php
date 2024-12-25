a118e19a864686809c9fe15fef77faf8b9f8cfaf
Mounir <lm_melzi@esi.dz>
2024-12-25 21:11:19 +0100

    create base model class

--- app/core/Model.php
+++ app/core/Model.php
@@ -2,4 +2,50 @@
 
 namespace App\Core;
 
-abstract class Model {}
+use App\Core\Database;
+use App\Utils\Query;
+
+abstract class Model
+{
+    public array $data;
+    protected Query $query;
+
+    public function __construct(array $data = [])
+    {
+        $this->data = $data;
+        $this->query = new Query(Database::getInstance());
+    }
+
+    public function all(): array
+    {
+        return $this->query->getAll();
+    }
+
+    public function get(int $id): mixed
+    {
+        return $this->query->getById($id);
+    }
+
+    public function validate(): bool
+    {
+        return true;
+    }
+
+    public function save(): bool
+    {
+        if (!$this->validate()) {
+            return false;
+        }
+
+        if (isset($this->data["id"]) && (self::get($this->data["id"]) !== null)) {
+            return $this->query->update($this->data);
+        } else {
+            return $this->query->insert($this->data);
+        }
+    }
+
+    public function delete(): bool
+    {
+        return $this->query->delete($this->data["id"]);
+    }
+}
